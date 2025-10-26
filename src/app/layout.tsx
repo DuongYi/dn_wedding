b535557adb93ca683d1df12a4918e54fc355bbf6
Vu Duc Duong <duongvd7@viettel.com.vn>
2025-10-26 14:49:13 +0700

    update layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -49,6 +49,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
         <meta name="description" content="CopyRight Dương & Kim Ngân 2025" />
         <link rel="icon" href="/asset/pictures/icon/favicon.ico" />
       </head>
